4da69578c22253be460729009b66d3ae5f5e71d1
uzairak12 <uzairak12@tamu.edu>
2025-09-03 15:41:04 -0500

    Modified database to include all 4 data fields for a book. Reflected changes in the Create aspect.

--- db/migrate/20250829210507_create_books.rb
+++ db/migrate/20250829210507_create_books.rb
@@ -2,6 +2,9 @@ class CreateBooks < ActiveRecord::Migration[6.1]
   def change
     create_table :books do |t|
       t.string :title
+      t.string :author
+      t.decimal :price, precision: 6, scale: 2
+      t.date :date_published
 
       t.timestamps
     end
