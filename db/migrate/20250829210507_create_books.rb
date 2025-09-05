1fdaa3fec68b3ea505efd9cf543eb60f8ea413ec
uzairak12 <uzairak12@tamu.edu>
2025-09-05 17:14:31 -0500

    Made changes to take out extra attributes

--- db/migrate/20250829210507_create_books.rb
+++ db/migrate/20250829210507_create_books.rb
@@ -2,9 +2,9 @@ class CreateBooks < ActiveRecord::Migration[6.1]
   def change
     create_table :books do |t|
       t.string :title
-      t.string :author
-      t.decimal :price, precision: 6, scale: 2
-      t.date :date_published
+      #t.string :author
+      #t.decimal :price, precision: 6, scale: 2
+      #t.date :date_published
 
       t.timestamps
     end
